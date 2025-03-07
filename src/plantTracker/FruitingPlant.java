c499ac0a8a891bfe64c442e6ab8a67cbeee3e7d2
madeleineclay <madeleine.clay@liferay.com>
2025-03-06 19:48:31 -0600

    added FruitingPlant and Vegetable

--- src/plantTracker/FruitingPlant.java
+++ src/plantTracker/FruitingPlant.java
@@ -1,5 +1,30 @@
 package plantTracker;
 
 public class FruitingPlant extends Plant {
+	public String name;
+	public String scientificName;
+	public String fruitType;
+	public String sunlightRequirement;
+	public String waterRequirement;
+	public String growthSeason;
 
+	public FruitingPlant(String name, String scientificName, String fruitType, String sunlightRequirement,
+			String waterRequirement, String growthSeason) {
+		this.name = name;
+		this.scientificName = scientificName;
+		this.fruitType = fruitType;
+		this.sunlightRequirement = sunlightRequirement;
+		this.waterRequirement = waterRequirement;
+		this.growthSeason = growthSeason;
+	}
+
+	public void displayInfo() {
+		System.out.println("Fruiting Plant: " + name);
+		System.out.println("Scientific Name: " + scientificName);
+		System.out.println("Fruit Type: " + fruitType);
+		System.out.println("Sunlight Requirement: " + sunlightRequirement);
+		System.out.println("Water Requirement: " + waterRequirement);
+		System.out.println("Growth Season: " + growthSeason);
+		System.out.println("---------------------------");
+	}
 }
--- src/plantTracker/Vegetable.java
+++ src/plantTracker/Vegetable.java
@@ -1,5 +1,27 @@
 package plantTracker;
 
 public class Vegetable extends Plant {
+	private String vegetable;
 
+	public Vegetable() {
+		super();
+		this.vegetable = "";
+	}
+
+	public Vegetable(String name, String species, String vegetable) {
+		super(name, species);
+		this.vegetable = vegetable;
+	}
+
+	public String getVegetable() {
+		return vegetable;
+	}
+
+	public void setVegetable(String vegetable) {
+		this.vegetable = vegetable;
+	}
+
+	public int getAge() {
+		return 0;
+	}
 }
