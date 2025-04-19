e7dcce05f05af76b4bc609f0ea678b0341a99421
madeleineclay <madeleine.clay@liferay.com>
2025-04-19 11:57:38 -0500

    foundations of user integration

--- src/plantTracker/User.java
+++ src/plantTracker/User.java
@@ -2,4 +2,38 @@ package plantTracker;
 
 public class User {
 
+	private String username;
+	private String password;
+	private boolean admin;
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+		this.admin = false;
+	}
+
+	protected String getPassword() {
+		return password;
+	}
+
+	protected void setPassword(String password) {
+		this.password = password;
+	}
+
+	protected String getUsername() {
+		return username;
+	}
+
+	protected void setUsername(String username) {
+		this.username = username;
+	}
+
+	protected boolean isAdmin() {
+		return admin;
+	}
+
+	protected void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
 }
